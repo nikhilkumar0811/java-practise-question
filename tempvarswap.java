59688db4a7fcdb1bc6c7a773dfac5643538d9538
Nikhil Kumar <nikhilkumar08112000@gmail.com>
2024-09-28 22:09:44 +0530

    swapvarnumber in java

---
+++ tempvarswap.java
@@ -0,0 +1,13 @@
+public class tempvarswap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
